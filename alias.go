ec2a527976c96158700662928c2c1c6eb2c5c778
Raúl Kripalani <raul@protocol.ai>
2019-04-22 11:44:49 +0100

    add top-level aliases for central primitives of go-libp2p.

---
+++ alias.go
@@ -0,0 +1,54 @@
+// Package core provides aliases for foundational, central go-libp2p primitives for convenient access.
+package core
+
+import (
+	"github.com/libp2p/go-libp2p-core/host"
+	"github.com/libp2p/go-libp2p-core/network"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/protocol"
+	"github.com/multiformats/go-multiaddr"
+)
+
+// ENTITIES.
+
+// Multiaddr aliases the Multiaddr type from github.com/multiformats/go-multiaddr.
+//
+// Refer to the docs on that type for more info.
+type Multiaddr = multiaddr.Multiaddr
+
+// PeerID aliases peer.ID.
+//
+// Refer to the docs on that type for more info.
+type PeerID = peer.ID
+
+// PeerID aliases protocol.ID.
+//
+// Refer to the docs on that type for more info.
+type ProtocolID = protocol.ID
+
+// PeerInfo aliases peer.Info.
+//
+// Refer to the docs on that type for more info.
+type PeerInfo = peer.Info
+
+// CONSTRUCTIONS.
+
+// Host aliases host.Host.
+//
+// Refer to the docs on that type for more info.
+type Host = host.Host
+
+// Network aliases network.Network.
+//
+// Refer to the docs on that type for more info.
+type Network = network.Network
+
+// Conn aliases network.Conn.
+//
+// Refer to the docs on that type for more info.
+type Conn = network.Conn
+
+// Stream aliases network.Stream.
+//
+// Refer to the docs on that type for more info.
+type Stream = network.Stream
