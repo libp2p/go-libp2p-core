5e12adb20466bbb2a41afaa0f2b75a9637dec802
Yusef Napora <yusef@napora.org>
2019-05-02 10:32:08 -0400

    fix incorrect docs for Negotiator & add docs for Negotiator methods

--- protocol/switch.go
+++ protocol/switch.go
@@ -16,6 +16,11 @@ type HandlerFunc = func(protocol string, rwc io.ReadWriteCloser) error
 // Router is an interface that allows users to add and remove protocol handlers,
 // which will be invoked when incoming stream requests for registered protocols
 // are accepted.
+//
+// Upon receiving an incoming stream request, the Router will check all registered
+// protocol handlers to determine which (if any) is capable of handling the stream.
+// The handlers are checked in order of registration; if multiple handlers are
+// eligible, only the first to be registered will be invoked.
 type Router interface {
 
 	// AddHandler registers the given handler to be invoked for
@@ -23,12 +28,13 @@ type Router interface {
 	AddHandler(protocol string, handler HandlerFunc)
 
 	// AddHandlerWithFunc registers the given handler to be invoked
-	// for exact literal matches of the given protocol ID string,
-	// **or** when the provided match function returns true.
+	// when the provided match function returns true.
 	//
 	// The match function will be invoked with an incoming protocol
-	// ID string when the router is unable to find an exact literal
-	// match among the registered handlers.
+	// ID string, and should return true if the handler supports
+	// the protocol. Note that the protocol ID argument is not
+	// used for matching; if you want to match the protocol ID
+	// string exactly, you must check for it in your match function.
 	AddHandlerWithFunc(protocol string, match func(string) bool, handler HandlerFunc)
 
 	// RemoveHandler removes the registered handler (if any) for the
@@ -43,13 +49,27 @@ type Router interface {
 }
 
 // Negotiator is a component capable of reaching agreement over what protocols
-// to use for a given stream of communication.
-//
-// The Negotiator is responsible for proposing the protocol to use for a given
-// stream, and it responds to proposals sent by the other end of the stream.
+// to use for inbound streams of communication.
 type Negotiator interface {
+
+	// NegotiateLazy will return the registered protocol handler to use
+	// for a given inbound stream, returning as soon as the protocol has been
+	// determined. Returns an error if negotiation fails.
+	//
+	// NegotiateLazy may return before all protocol negotiation responses have been
+	// written to the stream. This is in contrast to Negotiate, which will block until
+	// the Negotiator is finished with the stream.
 	NegotiateLazy(rwc io.ReadWriteCloser) (io.ReadWriteCloser, string, HandlerFunc, error)
+
+	// Negotiate will return the registered protocol handler to use for a given
+	// inbound stream, returning after the protocol has been determined and the
+	// Negotiator has finished using the stream for negotiation. Returns an
+	// error if negotiation fails.
 	Negotiate(rwc io.ReadWriteCloser) (string, HandlerFunc, error)
+
+	// Handle calls Negotiate to determine which protocol handler to use for an
+	// inbound stream, then invokes the protocol handler function, passing it
+	// the protocol ID and the stream. Returns an error if negotiation fails.
 	Handle(rwc io.ReadWriteCloser) error
 }
 
