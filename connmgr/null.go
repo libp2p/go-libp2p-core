a69990cf24970d64747d912d8da8324cfd586949
vyzo <vyzo@hackzen.org>
2019-05-18 21:07:14 +0300

    add Close to interface, implement in nullconnmgr

--- connmgr/null.go
+++ connmgr/null.go
@@ -12,10 +12,12 @@ type NullConnMgr struct{}
 
 var _ ConnManager = (*NullConnMgr)(nil)
 
-func (_ NullConnMgr) TagPeer(peer.ID, string, int)      {}
-func (_ NullConnMgr) UntagPeer(peer.ID, string)         {}
-func (_ NullConnMgr) GetTagInfo(peer.ID) *TagInfo       { return &TagInfo{} }
-func (_ NullConnMgr) TrimOpenConns(ctx context.Context) {}
-func (_ NullConnMgr) Notifee() network.Notifiee         { return network.GlobalNoopNotifiee }
-func (_ NullConnMgr) Protect(peer.ID, string)           {}
-func (_ NullConnMgr) Unprotect(peer.ID, string) bool    { return false }
+func (_ NullConnMgr) TagPeer(peer.ID, string, int)             {}
+func (_ NullConnMgr) UntagPeer(peer.ID, string)                {}
+func (_ NullConnMgr) UpsertTag(peer.ID, string, func(int) int) {}
+func (_ NullConnMgr) GetTagInfo(peer.ID) *TagInfo              { return &TagInfo{} }
+func (_ NullConnMgr) TrimOpenConns(ctx context.Context)        {}
+func (_ NullConnMgr) Notifee() network.Notifiee                { return network.GlobalNoopNotifiee }
+func (_ NullConnMgr) Protect(peer.ID, string)                  {}
+func (_ NullConnMgr) Unprotect(peer.ID, string) bool           { return false }
+func (_ NullConnMgr) Close() error                             { return nil }
