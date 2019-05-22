f5a63f2eac958bb9858dcf3394113ff553bd8986
Raúl Kripalani <raul@protocol.ai>
2019-05-22 17:59:26 +0100

    sync peerstore interface changes.

--- go.mod
+++ go.mod
@@ -12,5 +12,6 @@ require (
 	github.com/mr-tron/base58 v1.1.1
 	github.com/multiformats/go-multiaddr v0.0.2
 	github.com/multiformats/go-multihash v0.0.1
+	github.com/spacemonkeygo/openssl v0.0.0-20181017203307-c2dcc5cca94a
 	golang.org/x/crypto v0.0.0-20190225124518-7f87c0fbb88b
 )
--- peerstore/peerstore.go
+++ peerstore/peerstore.go
@@ -59,16 +59,12 @@ type Peerstore interface {
 	KeyBook
 	PeerMetadata
 	Metrics
+	ProtoBook
 
 	// PeerInfo returns a peer.PeerInfo struct for given peer.ID.
 	// This is a small slice of the information Peerstore has on
 	// that peer, useful to other services.
 	PeerInfo(peer.ID) peer.AddrInfo
-
-	GetProtocols(peer.ID) ([]string, error)
-	AddProtocols(peer.ID, ...string) error
-	SetProtocols(peer.ID, ...string) error
-	SupportsProtocols(peer.ID, ...string) ([]string, error)
 
 	// Peers returns all of the peer IDs stored across all inner stores.
 	Peers() peer.IDSlice
@@ -155,4 +151,12 @@ type Metrics interface {
 	// LatencyEWMA returns an exponentially-weighted moving avg.
 	// of all measurements of a peer's latency.
 	LatencyEWMA(peer.ID) time.Duration
+}
+
+// ProtoBook tracks the protocols supported by peers
+type ProtoBook interface {
+	GetProtocols(peer.ID) ([]string, error)
+	AddProtocols(peer.ID, ...string) error
+	SetProtocols(peer.ID, ...string) error
+	SupportsProtocols(peer.ID, ...string) ([]string, error)
 }
