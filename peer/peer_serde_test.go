51df94ba8305f0f6786fe62450fe16e20b2f2513
Raúl Kripalani <raul@protocol.ai>
2019-05-23 15:59:42 +0100

    absorb {crypto,peer} test utils.
    Avoids circular module dependency.

--- peer/peer_serde_test.go
+++ peer/peer_serde_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 
 	"github.com/libp2p/go-libp2p-core/peer"
-	. "github.com/libp2p/go-libp2p-testing/peer"
+	. "github.com/libp2p/go-libp2p-core/test"
 )
 
 func TestPeerSerdePB(t *testing.T) {
