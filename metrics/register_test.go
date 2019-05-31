3a0767210a17b93c7002b9617384a27ee59a0fa6
Mantas Vidutis <mantas.a.vidutis@gmail.com>
2019-05-31 11:28:14 -0700

    add failing test

--- metrics/register_test.go
+++ metrics/register_test.go
@@ -15,8 +15,8 @@ func newTestMeasure(name string) stats.Measure {
 	)
 }
 
-func newTestView(name string) view.View {
-	return view.View{
+func newTestView(name string) *view.View {
+	return &view.View{
 		Name:        fmt.Sprintf("test/%s", name),
 		Description: fmt.Sprintf("Test view %s", name),
 		Measure:     newTestMeasure(name),
@@ -25,14 +25,12 @@ func newTestView(name string) view.View {
 }
 
 func TestRegisteringViews(t *testing.T) {
-	for k, _ := range registeredViews {
-		delete(registeredViews, k)
-	}
+	registeredViews = make(map[string][]*view.View)
 
 	t.Run("test registering first views", func(t *testing.T) {
 		testView := newTestView("empty-map-0")
 
-		if err := RegisterViews("test", &testView); err != nil {
+		if err := RegisterViews("test", testView); err != nil {
 			t.Fatal("unable to register view in empty map", err)
 		}
 	})
@@ -41,10 +39,10 @@ func TestRegisteringViews(t *testing.T) {
 		testView := newTestView("empty-map-1")
 		testView2 := newTestView("existing-entity-0")
 
-		if err := RegisterViews("test2", &testView); err != nil {
+		if err := RegisterViews("test2", testView); err != nil {
 			t.Fatal("unable to register view in empty map", err)
 		}
-		if err := RegisterViews("test3", &testView2); err != nil {
+		if err := RegisterViews("test3", testView2); err != nil {
 			t.Fatal("unable to register view in map", err)
 		}
 	})
@@ -53,10 +51,10 @@ func TestRegisteringViews(t *testing.T) {
 		testView := newTestView("empty-map-2")
 		testView2 := newTestView("existing-entity-1")
 
-		if err := RegisterViews("test4", &testView); err != nil {
+		if err := RegisterViews("test4", testView); err != nil {
 			t.Fatal("unable to register view in empty map", err)
 		}
-		if err := RegisterViews("test4", &testView2); err == nil {
+		if err := RegisterViews("test4", testView2); err == nil {
 			t.Fatal("allowed duplicate view registration")
 		}
 	})
@@ -64,7 +62,7 @@ func TestRegisteringViews(t *testing.T) {
 	t.Run("test looking up views", func(t *testing.T) {
 		testView := newTestView("empty-map-3")
 
-		if err := RegisterViews("test5", &testView); err != nil {
+		if err := RegisterViews("test5", testView); err != nil {
 			t.Fatal("unable to register view in empty map", err)
 		}
 
@@ -75,6 +73,19 @@ func TestRegisteringViews(t *testing.T) {
 
 		if views[0].Name != testView.Name {
 			t.Fatal("incorrect view lookup, received name:", views[0].Name)
+		}
+	})
+
+	t.Run("test retrieving all views", func(t *testing.T) {
+		views := []*view.View{newTestView("all-views-0"), newTestView("all-views-1"), newTestView("all-views-2")}
+
+		if err := RegisterViews("test6", views...); err != nil {
+			t.Fatal("unable to register multiple views at once", err)
+		}
+
+		allViews := AllViews()
+		if len(allViews) != len(views) {
+			t.Fatalf("didn't receive equal number of views: %d %d", len(views), len(allViews))
 		}
 	})
 }
