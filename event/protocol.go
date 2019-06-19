eee9281d0b6bf9f7fe62af1c55e2b5ffc4039657
Raúl Kripalani <raul@protocol.ai>
2019-06-19 17:44:58 +0100

    add event for local protocols update.

--- event/protocol.go
+++ event/protocol.go
@@ -14,3 +14,13 @@ type EvtPeerProtocolsUpdated struct {
 	// Removed enumerates the protocols that were removed by this peer.
 	Removed []protocol.ID
 }
+
+// EvtLocalProtocolsUpdated should be emitted when stream handlers are attached or detached from the local host.
+// For handlers attached with a matcher predicate (host.SetStreamHandlerMatch()), only the protocol ID will be
+// included in this event.
+type EvtLocalProtocolsUpdated struct {
+	// Added enumerates the protocols that were added locally.
+	Added []protocol.ID
+	// Removed enumerates the protocols that were removed locally.
+	Removed []protocol.ID
+}
