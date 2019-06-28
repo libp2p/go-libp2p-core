0adbd50288bb17986cec2d151c55ceb3c237c221
whyrusleeping <why@ipfs.io>
2019-06-28 12:29:12 +0200

    implement caching for rsaKey.Bytes()

--- crypto/key.go
+++ crypto/key.go
@@ -290,7 +290,22 @@ func PublicKeyFromProto(pmes *pb.PublicKey) (PubKey, error) {
 		return nil, ErrBadKeyType
 	}
 
-	return um(pmes.GetData())
+	data := pmes.GetData()
+
+	pk, err := um(data)
+	if err != nil {
+		return nil, err
+	}
+
+	switch tpk := pk.(type) {
+	case *RsaPublicKey:
+		bcopy := make([]byte, len(data))
+		copy(bcopy, data)
+
+		tpk.cached = bcopy
+	}
+
+	return pk, nil
 }
 
 // MarshalPublicKey converts a public key object into a protobuf serialized
