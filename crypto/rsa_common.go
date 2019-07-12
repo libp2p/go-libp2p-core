c817d49d02971a47dbaffb335504cc4ea5228b5a
Cole Brown <bigswim@gmail.com>
2019-07-12 16:04:56 -0400

    Add env flag for allowing unsafe rsa keys in tests

--- crypto/rsa_common.go
+++ crypto/rsa_common.go
@@ -2,11 +2,24 @@ package crypto
 
 import (
 	"fmt"
+	"os"
 )
 
-const MinRsaKeyBits = 2048
+// UnsafeRsaKeyEnv is an environment variable which, when set, lowers the
+// minimum required bits of RSA keys to 512. This should be used exclusively in
+// test situations.
+const UnsafeRsaKeyEnv = "LIBP2P_ALLOW_UNSAFE_RSA_KEYS"
+
+var MinRsaKeyBits = 2048
 
 // ErrRsaKeyTooSmall is returned when trying to generate or parse an RSA key
-// that's smaller than 512 bits. Keys need to be larger enough to sign a 256bit
-// hash so this is a reasonable absolute minimum.
-var ErrRsaKeyTooSmall = fmt.Errorf("rsa keys must be >= %d bits to be useful", MinRsaKeyBits)
+// that's smaller than MinRsaKeyBits bits. In test
+var ErrRsaKeyTooSmall error
+
+func init() {
+	if _, ok := os.LookupEnv(UnsafeRsaKeyEnv); ok {
+		MinRsaKeyBits = 512
+	}
+
+	ErrRsaKeyTooSmall = fmt.Errorf("rsa keys must be >= %d bits to be useful", MinRsaKeyBits)
+}
