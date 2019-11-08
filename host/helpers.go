b8de7fa7f340c2f30fbd15051ab54f290fcae19f
Yusef Napora <yusef@protocol.ai>
2019-11-08 11:09:02 -0500

    add helpers to make routing records for Host

--- host/helpers.go
+++ host/helpers.go
@@ -1,6 +1,11 @@
 package host
 
-import "github.com/libp2p/go-libp2p-core/peer"
+import (
+	"errors"
+	"github.com/libp2p/go-libp2p-core/crypto"
+	"github.com/libp2p/go-libp2p-core/peer"
+	"github.com/libp2p/go-libp2p-core/routing"
+)
 
 // InfoFromHost returns a peer.AddrInfo struct with the Host's ID and all of its Addrs.
 func InfoFromHost(h Host) *peer.AddrInfo {
@@ -8,4 +13,20 @@ func InfoFromHost(h Host) *peer.AddrInfo {
 		ID:    h.ID(),
 		Addrs: h.Addrs(),
 	}
+}
+
+// RoutingStateFromHost returns a routing.RoutingState record that contains the Host's
+// ID and all of its listen Addrs.
+func RoutingStateFromHost(h Host) *routing.RoutingState {
+	return routing.RoutingStateFromAddrInfo(InfoFromHost(h))
+}
+
+// SignedRoutingStateFromHost
+func SignedRoutingStateFromHost(h Host) (*crypto.SignedEnvelope, error) {
+	privKey := h.Peerstore().PrivKey(h.ID())
+	if privKey == nil {
+		return nil, errors.New("unable to find host's private key in peerstore")
+	}
+
+	return RoutingStateFromHost(h).ToSignedEnvelope(privKey)
 }
