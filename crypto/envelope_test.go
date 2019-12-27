6028ba0bbafdb805dac34af1f0cc62156ecb7c59
Raúl Kripalani <raul@protocol.ai>
2019-12-27 20:07:28 +0000

    envelope: use byte slices from pool; adjust interface.

--- crypto/envelope_test.go
+++ crypto/envelope_test.go
@@ -12,11 +12,15 @@ import (
 
 // Make an envelope, verify & open it, marshal & unmarshal it
 func TestEnvelopeHappyPath(t *testing.T) {
-	priv, pub, err := test.RandTestKeyPair(Ed25519, 256)
+	var (
+		payload        = []byte("happy hacking")
+		domain         = "libp2p-testing"
+		payloadType    = []byte("/libp2p/testdata")
+		priv, pub, err = test.RandTestKeyPair(Ed25519, 256)
+	)
+
 	test.AssertNilError(t, err)
-	payload := []byte("happy hacking")
-	domain := "libp2p-testing"
-	payloadType := []byte("/libp2p/testdata")
+
 	envelope, err := MakeEnvelope(priv, domain, payloadType, payload)
 	test.AssertNilError(t, err)
 
@@ -30,7 +34,8 @@ func TestEnvelopeHappyPath(t *testing.T) {
 
 	serialized, err := envelope.Marshal()
 	test.AssertNilError(t, err)
-	deserialized, err := OpenEnvelope(serialized, domain)
+
+	deserialized, err := ConsumeEnvelope(serialized, domain)
 	test.AssertNilError(t, err)
 
 	if bytes.Compare(deserialized.Payload, payload) != 0 {
@@ -43,60 +48,81 @@ func TestEnvelopeHappyPath(t *testing.T) {
 }
 
 func TestMakeEnvelopeFailsWithEmptyDomain(t *testing.T) {
-	priv, _, err := test.RandTestKeyPair(Ed25519, 256)
+	var (
+		payload      = []byte("happy hacking")
+		payloadType  = []byte("/libp2p/testdata")
+		priv, _, err = test.RandTestKeyPair(Ed25519, 256)
+	)
+
 	if err != nil {
-		t.Error(err)
+		t.Fatal(err)
 	}
-	payload := []byte("happy hacking")
-	payloadType := []byte("/libp2p/testdata")
+
 	_, err = MakeEnvelope(priv, "", payloadType, payload)
 	test.ExpectError(t, err, "making an envelope with an empty domain should fail")
 }
 
 func TestEnvelopeValidateFailsForDifferentDomain(t *testing.T) {
-	priv, _, err := test.RandTestKeyPair(Ed25519, 256)
+	var (
+		payload      = []byte("happy hacking")
+		domain       = "libp2p-testing"
+		payloadType  = []byte("/libp2p/testdata")
+		priv, _, err = test.RandTestKeyPair(Ed25519, 256)
+	)
+
 	test.AssertNilError(t, err)
-	payload := []byte("happy hacking")
-	domain := "libp2p-testing"
-	payloadType := []byte("/libp2p/testdata")
+
 	envelope, err := MakeEnvelope(priv, domain, payloadType, payload)
 	test.AssertNilError(t, err)
+
 	serialized, err := envelope.Marshal()
+
 	// try to open our modified envelope
-	_, err = OpenEnvelope(serialized, "wrong-domain")
+	_, err = ConsumeEnvelope(serialized, "wrong-domain")
 	test.ExpectError(t, err, "should not be able to open envelope with incorrect domain")
 }
 
 func TestEnvelopeValidateFailsIfTypeHintIsAltered(t *testing.T) {
-	priv, _, err := test.RandTestKeyPair(Ed25519, 256)
+	var (
+		payload      = []byte("happy hacking")
+		domain       = "libp2p-testing"
+		payloadType  = []byte("/libp2p/testdata")
+		priv, _, err = test.RandTestKeyPair(Ed25519, 256)
+	)
+
 	test.AssertNilError(t, err)
-	payload := []byte("happy hacking")
-	domain := "libp2p-testing"
-	payloadType := []byte("/libp2p/testdata")
+
 	envelope, err := MakeEnvelope(priv, domain, payloadType, payload)
 	test.AssertNilError(t, err)
+
 	serialized := alterMessageAndMarshal(t, envelope, func(msg *pb.SignedEnvelope) {
 		msg.PayloadType = []byte("foo")
 	})
+
 	// try to open our modified envelope
-	_, err = OpenEnvelope(serialized, domain)
+	_, err = ConsumeEnvelope(serialized, domain)
 	test.ExpectError(t, err, "should not be able to open envelope with modified PayloadType")
 }
 
 func TestEnvelopeValidateFailsIfContentsAreAltered(t *testing.T) {
-	priv, _, err := test.RandTestKeyPair(Ed25519, 256)
+	var (
+		payload      = []byte("happy hacking")
+		domain       = "libp2p-testing"
+		payloadType  = []byte("/libp2p/testdata")
+		priv, _, err = test.RandTestKeyPair(Ed25519, 256)
+	)
+
 	test.AssertNilError(t, err)
-	payload := []byte("happy hacking")
-	domain := "libp2p-testing"
-	payloadType := []byte("/libp2p/testdata")
+
 	envelope, err := MakeEnvelope(priv, domain, payloadType, payload)
 	test.AssertNilError(t, err)
 
 	serialized := alterMessageAndMarshal(t, envelope, func(msg *pb.SignedEnvelope) {
 		msg.Payload = []byte("totally legit, trust me")
 	})
+
 	// try to open our modified envelope
-	_, err = OpenEnvelope(serialized, domain)
+	_, err = ConsumeEnvelope(serialized, domain)
 	test.ExpectError(t, err, "should not be able to open envelope with modified payload")
 }
 
@@ -107,13 +133,17 @@ func TestEnvelopeValidateFailsIfContentsAreAltered(t *testing.T) {
 // Returns the serialized altered protobuf message.
 func alterMessageAndMarshal(t *testing.T, envelope *SignedEnvelope, alterMsg func(*pb.SignedEnvelope)) []byte {
 	t.Helper()
+
 	serialized, err := envelope.Marshal()
 	test.AssertNilError(t, err)
+
 	msg := pb.SignedEnvelope{}
 	err = proto.Unmarshal(serialized, &msg)
 	test.AssertNilError(t, err)
+
 	alterMsg(&msg)
 	serialized, err = msg.Marshal()
 	test.AssertNilError(t, err)
+
 	return serialized
 }
