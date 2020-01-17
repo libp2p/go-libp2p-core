d567b244773bddfec380884f686620aa98b87684
Yusef Napora <yusef@protocol.ai>
2020-01-17 10:14:21 -0500

    rename CertifiedAddrBook methods, update comments

--- peerstore/peerstore.go
+++ peerstore/peerstore.go
@@ -123,25 +123,33 @@ type AddrBook interface {
 
 // CertifiedAddrBook manages "self-certified" addresses for remote peers.
 // Self-certified addresses are contained in peer.PeerRecords
-// that are wraped in a record.Envelope and signed by the peer
+// which are wrapped in a record.Envelope and signed by the peer
 // to whom they belong.
+//
+// Certified addresses (CA) are generally more secure than uncertified
+// addresses (UA). Consequently, CAs beat and displace UAs. When the
+// peerstore learns CAs for a peer, it will reject UAs for the same peer
+// (as long as the former haven't expired).
+// Furthermore, peer records act like sequenced snapshots of CAs. Therefore,
+// processing a peer record that's newer than the last one seen overwrites
+// all addresses with the incoming ones.
 //
 // This interface is most useful when combined with AddrBook.
 // To test whether a given AddrBook / Peerstore implementation supports
-// certified addresses, callers should type-assert on the CertifiedAddrBook
-// interface:
+// certified addresses, callers should use the GetCertifiedAddrBook helper or
+// type-assert on the CertifiedAddrBook interface:
 //
 //     if cab, ok := aPeerstore.(CertifiedAddrBook); ok {
-//         cab.AddCertifiedAddrs(signedPeerRecord, aTTL)
+//         cab.ProcessPeerRecord(signedPeerRecord, aTTL)
 //     }
 //
 type CertifiedAddrBook interface {
-	// AddCertifiedAddrs adds addresses from a signed peer.PeerRecord (contained in
+	// ProcessPeerRecord adds addresses from a signed peer.PeerRecord (contained in
 	// a routing.Envelope), which will expire after the given TTL.
 	//
 	// Signed records added via this method will be stored without
-	// alteration as long as the address TTLs remain valid. The SignedEnvelopes
-	// containing the PeerRecords can be retrieved by calling SignedPeerRecord(peerID).
+	// alteration as long as the address TTLs remain valid. The Envelopes
+	// containing the PeerRecords can be retrieved by calling GetPeerRecord(peerID).
 	//
 	// If the signed PeerRecord belongs to a peer that already has certified
 	// addresses in the CertifiedAddrBook, the new addresses will replace the
@@ -153,19 +161,19 @@ type CertifiedAddrBook interface {
 	// If the CertifiedAddrBook is also an AddrBook (which is most likely the case),
 	// adding certified addresses for a peer will *replace* any
 	// existing non-certified addresses for that peer, and only the certified
-	// addresses will be returned from AddrBook.Addrs.
+	// addresses will be returned from AddrBook.Addrs thereafter.
 	//
 	// Likewise, once certified addresses have been added for a given peer,
 	// any non-certified addresses added via AddrBook.AddAddrs or
 	// AddrBook.SetAddrs will be ignored. AddrBook.SetAddrs may still be used
-	// to update the TTL of certified addresses if they have previously been
-	// added via AddCertifiedAddrs.
-	AddCertifiedAddrs(s *record.Envelope, ttl time.Duration) error
+	// to update the TTL of certified addresses that have previously been
+	// added via ProcessPeerRecord.
+	ProcessPeerRecord(s *record.Envelope, ttl time.Duration) error
 
-	// SignedPeerRecord returns a Envelope containing a PeerRecord for the
+	// GetPeerRecord returns a Envelope containing a PeerRecord for the
 	// given peer id, if one exists.
 	// Returns nil if no signed PeerRecord exists for the peer.
-	SignedPeerRecord(p peer.ID) *record.Envelope
+	GetPeerRecord(p peer.ID) *record.Envelope
 }
 
 // GetCertifiedAddrBook is a helper to "upcast" an AddrBook to a
