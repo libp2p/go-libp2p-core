1a439d73e7b757c1301dc75315a99e4f94b299c9
Yusef Napora <yusef@protocol.ai>
2020-02-03 16:50:13 -0500

    fix import

--- host/helpers.go
+++ host/helpers.go
@@ -1,8 +1,6 @@
 package host
 
-import (
-	"github.com/libp2p/go-libp2p-core/peer"
-)
+import "github.com/libp2p/go-libp2p-core/peer"
 
 // InfoFromHost returns a peer.AddrInfo struct with the Host's ID and all of its Addrs.
 func InfoFromHost(h Host) *peer.AddrInfo {
