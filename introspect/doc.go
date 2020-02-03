e84650d6db6c38f07f2630b122fddc2c60ba94f8
Raúl Kripalani <raul@protocol.ai>
2020-02-03 16:49:08 +0000

    Update doc.go

--- introspect/doc.go
+++ introspect/doc.go
@@ -1,5 +1,5 @@
-// Package introspect is EXPERIMENTAL. It is subject to heavy change, it it WILL
-// change. For now, it is the simplest implementation to power the
+// Package introspect is EXPERIMENTAL. It is subject to heavy change, and it
+// WILL change. For now, it is the simplest implementation to power the
 // proof-of-concept of the libp2p introspection framework.
 //
 // Package introspect contains the abstract skeleton of the introspection system
