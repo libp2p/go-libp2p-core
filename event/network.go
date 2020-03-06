493f2b140f27339cb83f26e91b772ba3aa23b6ee
Aarsh Shah <aarshkshah1992@gmail.com>
2020-03-06 16:08:03 +0530

    changes as per raul's review

--- event/network.go
+++ event/network.go
@@ -5,12 +5,12 @@ import (
 	"github.com/libp2p/go-libp2p-core/peer"
 )
 
-// EvtPeerConnectednessChange should be emitted every time we form a connection with a peer or drop our last
+// EvtPeerConnectednessChanged should be emitted every time we form a connection with a peer or drop our last
 // connection with the peer. Essentially, it is emitted in two cases:
 // a) We form a/any connection with a peer.
 // b) We go from having a connection/s with a peer to having no connection with the peer.
 // It contains the Id of the remote peer and the new connectedness state.
-type EvtPeerConnectednessChange struct {
-	RemotePeerId  peer.ID
+type EvtPeerConnectednessChanged struct {
+	Peer          peer.ID
 	Connectedness network.Connectedness
 }
