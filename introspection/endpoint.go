086efa616a18259a1846c93a93f1e0c6dd58f5dc
Aarsh Shah <aarshkshah1992@gmail.com>
2020-04-16 23:30:09 +0530

    pass evt bus

--- introspection/endpoint.go
+++ introspection/endpoint.go
@@ -1,5 +1,7 @@
 package introspection
 
+import "github.com/libp2p/go-libp2p-core/event"
+
 // Endpoint is the interface to be implemented by introspection
 // endpoints/servers.
 //
@@ -9,7 +11,9 @@ type Endpoint interface {
 	// Start starts the introspection endpoint. It must only be called once, and
 	// once the server is started, subsequent calls made without first calling
 	// Close will error.
-	Start() error
+	// It takes an event bus a parameter and uses it to subscribe to
+	// events that need to be pushed to the client.
+	Start(bus event.Bus) error
 
 	// Close stops the introspection endpoint. Calls to Close on an already
 	// closed endpoint, or an unstarted endpoint, must noop.
