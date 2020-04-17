b224967ed43f6a723ef1133fff656381cfe116fc
Aarsh Shah <aarshkshah1992@gmail.com>
2020-04-17 13:46:28 +0530

    JS String type

--- event/dht.go
+++ event/dht.go
@@ -1,10 +1,15 @@
 package event
 
+// TODO This is all hacky. Please don't judge the programmer on the
+// basis of any of this.
+
+type JSString string
+
 // TODO This is simply for being able to show DHT events on the UI for now.
 // DhtEvent is an event related to the DHT.
 type DhtEvent struct {
 	// EventType is the type of the event that has occured.
 	EventType string
 	// EventJson is the JSON representation of the event payload.
-	EventJson string
+	EventJson JSString
 }
