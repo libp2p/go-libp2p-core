e5edab11efb0b2e642ad1e679cf8084a9051d6c3
Raúl Kripalani <raul@protocol.ai>
2020-05-14 15:54:39 +0100

    connmgr: introduce abstractions and functions for decaying tags. (#104)

--- connmgr/manager.go
+++ connmgr/manager.go
@@ -14,14 +14,23 @@ import (
 	"github.com/libp2p/go-libp2p-core/peer"
 )
 
-// ConnManager tracks connections to peers, and allows consumers to associate metadata
-// with each peer.
-//
-// It enables connections to be trimmed based on implementation-defined heuristics.
-// The ConnManager allows libp2p to enforce an upper bound on the total number of
-// open connections.
-type ConnManager interface {
+// SupportsDecay evaluates if the provided ConnManager supports decay, and if
+// so, it returns the Decayer object. Refer to godocs on Decayer for more info.
+func SupportsDecay(mgr ConnManager) (Decayer, bool) {
+	d, ok := mgr.(Decayer)
+	return d, ok
+}
 
+// ConnManager tracks connections to peers, and allows consumers to associate
+// metadata with each peer.
+//
+// It enables connections to be trimmed based on implementation-defined
+// heuristics. The ConnManager allows libp2p to enforce an upper bound on the
+// total number of open connections.
+//
+// ConnManagers supporting decaying tags implement Decayer. Use the
+// SupportsDecay function to safely cast an instance to Decayer, if supported.
+type ConnManager interface {
 	// TagPeer tags a peer with a string, associating a weight with the tag.
 	TagPeer(peer.ID, string, int)
 
