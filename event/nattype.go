3267df19099a1ba68aa32103b13e925efeb2ef04
aarshkshah1992 <aarshkshah1992@gmail.com>
2021-01-27 20:13:36 +0530

    add transport protocol to NAT event

--- event/nattype.go
+++ event/nattype.go
@@ -2,7 +2,18 @@ package event
 
 import "github.com/libp2p/go-libp2p-core/network"
 
+// NATDeviceProtocol is the transport protocol for which the NAT Device Type has been determined.
+type NATDeviceProtocol int
+
+const (
+	// NATDeviceUDP means that the NAT Device Type has been determined for the UDP Protocol.
+	NATDeviceUDP NATDeviceProtocol = iota
+	// NATDeviceTCP means that the NAT Device Type has been determined for the TCP Protocol.
+	NATDeviceTCP
+)
+
 // EvtNATDeviceTypeChanged is an event struct to be emitted when the type of the NAT device changes.
 type EvtNATDeviceTypeChanged struct {
+	Protocol      NATDeviceProtocol
 	NatDeviceType network.NATDeviceType
 }
