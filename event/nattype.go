3723a852faa92486baf95367a8f39af15c9fab63
aarshkshah1992 <aarshkshah1992@gmail.com>
2021-01-28 15:10:21 +0530

    event changes

--- event/nattype.go
+++ event/nattype.go
@@ -2,18 +2,27 @@ package event
 
 import "github.com/libp2p/go-libp2p-core/network"
 
-// NATDeviceProtocol is the transport protocol for which the NAT Device Type has been determined.
-type NATDeviceProtocol int
+// NATTransportProtocol is the transport protocol for which the NAT Device Type has been determined.
+type NATTransportProtocol int
 
 const (
-	// NATDeviceUDP means that the NAT Device Type has been determined for the UDP Protocol.
-	NATDeviceUDP NATDeviceProtocol = iota
-	// NATDeviceTCP means that the NAT Device Type has been determined for the TCP Protocol.
-	NATDeviceTCP
+	// NATTransportUDP means that the NAT Device Type has been determined for the UDP Protocol.
+	NATTransportUDP NATTransportProtocol = iota
+	// NATTransportTCP means that the NAT Device Type has been determined for the TCP Protocol.
+	NATTransportTCP
 )
 
-// EvtNATDeviceTypeChanged is an event struct to be emitted when the type of the NAT device changes.
+// EvtNATDeviceTypeChanged is an event struct to be emitted when the type of the NAT device changes for a Transport Protocol.
+//
+// Note: This event is meaningful ONLY if the AutoNAT Reachability is Private.
+// Consumers of this event should ALSO consume the `EvtLocalReachabilityChanged` event and interpret
+// this event ONLY if the Reachability on the `EvtLocalReachabilityChanged` is Private.
 type EvtNATDeviceTypeChanged struct {
-	Protocol      NATDeviceProtocol
+	// TransportProtocol is the Transport Protocol for which the NAT Device Type has been determined.
+	TransportProtocol NATTransportProtocol
+	// NatDeviceType indicates the type of the NAT Device for the Transport Protocol.
+	// Currently, it can be either a `Cone NAT` or a `Symmetric NAT`. Please see the detailed documentation
+	// on `network.NATDeviceType` enumerations for a better understanding of what these types mean and
+	// how they impact Connectivity and Hole Punching.
 	NatDeviceType network.NATDeviceType
 }
