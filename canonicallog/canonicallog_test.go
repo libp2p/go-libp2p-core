b430f5b3a6a25b41b3138ee0d520e847c477ba37
Marco Munizaga <git@marcopolo.io>
2022-06-24 07:46:19 -0700

    Only log once if we failed to convert from netAddr (#264)

--- canonicallog/canonicallog_test.go
+++ canonicallog/canonicallog_test.go
@@ -2,6 +2,7 @@ package canonicallog
 
 import (
 	"fmt"
+	"net"
 	"testing"
 
 	"github.com/libp2p/go-libp2p-core/test"
@@ -10,10 +11,7 @@ import (
 
 func TestLogs(t *testing.T) {
 	LogMisbehavingPeer(test.RandPeerIDFatal(t), multiaddr.StringCast("/ip4/1.2.3.4"), "somecomponent", fmt.Errorf("something"), "hi")
-	LogMisbehavingPeerNetAddr(test.RandPeerIDFatal(t), dummyNetAddr{}, "somecomponent", fmt.Errorf("something"), "hi")
+
+	netAddr := &net.TCPAddr{IP: net.ParseIP("127.0.0.1"), Port: 80}
+	LogMisbehavingPeerNetAddr(test.RandPeerIDFatal(t), netAddr, "somecomponent", fmt.Errorf("something"), "hi")
 }
-
-type dummyNetAddr struct{}
-
-func (d dummyNetAddr) Network() string { return "tcp" }
-func (d dummyNetAddr) String() string  { return "127.0.0.1:80" }
