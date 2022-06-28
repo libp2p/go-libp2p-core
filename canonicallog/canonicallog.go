7fbd9a2f9406b51cdf70d0712dc82631976a0de2
Marco Munizaga <git@marcopolo.io>
2022-06-28 15:02:07 -0700

    Add canonical peer status log

--- canonicallog/canonicallog.go
+++ canonicallog/canonicallog.go
@@ -1,7 +1,9 @@
 package canonicallog
 
 import (
+	"math/rand"
 	"net"
+	"strings"
 
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/libp2p/go-libp2p-core/peer"
@@ -29,4 +31,27 @@ func LogMisbehavingPeerNetAddr(p peer.ID, peerAddr net.Addr, component string, o
 	}
 
 	log.Warnf("CANONICAL_MISBEHAVING_PEER: peer=%s addr=%s component=%s err=%v msg=%s", p, ma, component, originalErr, msg)
+}
+
+// LogPeerStatus logs any useful information about a peer. It takes in a sample
+// rate and will only log one in every sampleRate messages (randomly). This is
+// useful in surfacing events that are normal in isolation, but may be abnormal
+// in large quantities. For example, a successful connection from an IP address
+// is normal. 10,000 connections from that same IP address is not normal. libp2p
+// itself does nothing besides emitting this log. Hook this up to another tool
+// like fail2ban to action on the log.
+func LogPeerStatus(sampleRate int, p peer.ID, peerAddr multiaddr.Multiaddr, keyVals ...string) {
+	if rand.Intn(sampleRate) == 0 {
+		keyValsStr := strings.Builder{}
+		for i, kOrV := range keyVals {
+			if i%2 == 0 {
+				keyValsStr.WriteByte(' ')
+				keyValsStr.WriteString(kOrV)
+				keyValsStr.WriteByte('=')
+			} else {
+				keyValsStr.WriteString(kOrV)
+			}
+		}
+		log.Infof("CANONICAL_PEER_STATUS: peer=%s addr=%s sample_rate=%v%s", p, peerAddr.String(), sampleRate, keyValsStr.String())
+	}
 }
